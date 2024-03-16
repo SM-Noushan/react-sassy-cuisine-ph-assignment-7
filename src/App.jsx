df791b590fdefad2ae534af2f1780ec61c55ba5c
SMNoushan <noushan.md@gmail.com>
2024-03-17 03:24:56 +0600

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import Banner from "./components/header/Banner";
 import Navbar from "./components/header/Navbar";
 import Index from "./components/main/Index";
+import Footer from "./components/footer/Footer";
 function App() {
   return (
-    <div className="mx-auto max-w-[425px] md:container xl:max-w-screen-xl font-lexend text-haiti-950 p-4">
-      <header>
-        <Navbar />
-        <Banner />
-      </header>
-      <main>
-        <Index />
-      </main>
-    </div>
+    <>
+      <div className="mx-auto max-w-[425px] md:container xl:max-w-screen-xl font-lexend text-haiti-950 p-4">
+        <header>
+          <Navbar />
+          <Banner />
+        </header>
+        <main>
+          <Index />
+        </main>
+      </div>
+      <footer>
+        <Footer />
+      </footer>
+    </>
   )
 }
 
